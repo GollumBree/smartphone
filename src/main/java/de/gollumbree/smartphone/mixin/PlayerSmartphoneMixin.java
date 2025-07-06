79d18000561a8eb852dc08e2f1cbe585d1e11fec
GollumBree <haydn3lu.da@gmail.com>
2025-07-06 12:16:14 +0200

    simplified mixins for testing

--- src/main/java/de/gollumbree/smartphone/mixin/PlayerSmartphoneMixin.java
+++ src/main/java/de/gollumbree/smartphone/mixin/PlayerSmartphoneMixin.java
@@ -1,6 +1,7 @@
 package de.gollumbree.smartphone.mixin;
 
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,35 +12,42 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import de.gollumbree.smartphone.SmartphoneItem;
 
-import de.gollumbree.smartphone.api.PlayerSmartphoneExt;
+// import de.gollumbree.smartphone.api.PlayerSmartphoneExt;
 
-@Mixin(Player.class)
-public abstract class PlayerSmartphoneMixin implements PlayerSmartphoneExt {
+@Mixin(LivingEntity.class)
+public abstract class PlayerSmartphoneMixin // implements PlayerSmartphoneExt
+{
 
-    @Shadow
-    public abstract ItemStack getItemInHand(InteractionHand hand);
+    // @Shadow
+    // public abstract ItemStack getItemInHand(InteractionHand hand);
 
-    @Unique
-    private static final ThreadLocal<Boolean> ignoreSmartphoneFlag = ThreadLocal.withInitial(() -> false);
+    // @Unique
+    // private static final ThreadLocal<Boolean> ignoreSmartphoneFlag =
+    // ThreadLocal.withInitial(() -> false);
 
-    @Override
-    public ItemStack getItemInHand(InteractionHand hand, boolean ignoreSmartphone) {
-        ignoreSmartphoneFlag.set(ignoreSmartphone);
-        ItemStack result = getItemInHand(hand); // original method with injected swap
-        ignoreSmartphoneFlag.set(false);
-        return result;
-    }
+    // @Override
+    // public ItemStack getItemInHand(InteractionHand hand, boolean
+    // ignoreSmartphone) {
+    // ignoreSmartphoneFlag.set(ignoreSmartphone);
+    // ItemStack result = getItemInHand(hand); // original method with injected swap
+    // ignoreSmartphoneFlag.set(false);
+    // return result;
+    // }
 
     @Inject(method = "getItemInHand", at = @At("RETURN"), cancellable = true)
     private void onGetItemInHand(InteractionHand hand,
             CallbackInfoReturnable<ItemStack> cir) {
-        if (ignoreSmartphoneFlag.get())
-            return;
+        // if (ignoreSmartphoneFlag.get())
+        // return;
 
         ItemStack returned = cir.getReturnValue();
+        System.err.println("PlayerSmartphoneMixin.onGetItemInHand: " + returned);
         if (returned.getItem() instanceof SmartphoneItem) {
+            System.err
+                    .println("PlayerSmartphoneMixin.onGetItemInHand: Smartphone detected, looking for last used item.");
             var lookup = ((Player) (Object) this).level().registryAccess();
             ItemStack last = SmartphoneItem.getLastUsed(returned, lookup);
+            System.err.println("PlayerSmartphoneMixin.onGetItemInHand: Last used item: " + last);
             if (!last.isEmpty())
                 cir.setReturnValue(last);
         }
